835911e9598b022fe7d81206e9d4f6fd85f6461e
Clinton Scott <tox@localhost.localdomain>
2013-12-16 02:00:13 +1300

    Walking works Made the code look nicer maxThrust Issue unresolved.

--- main.go
+++ main.go
@@ -6,18 +6,19 @@ import (
 	"os"
 	"strings"
 	"strconv"
+	"path/filepath"
 )
 
 type Part struct {
 	mass float64
-	thrust int
+	thrust int64
 	name string
 	drag float64
 }
 
 type Stage struct {
 	mass float64
-	thrust int
+	thrust int64
 	drag float64
 	parts []Part
 }
@@ -25,79 +26,103 @@ type Stage struct {
 type Ship struct {
 	mass float64
 	weight float64
-	initThrust int
+	initThrust int64
 	twr float64
 	stages []Stage	
 }
 
 var config map[string]string
+var partsmap map[string]Part
+
+func errcheck(err error){
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	err := readconfig("config.stfu")
+	errcheck(err)
+
+	partslocation, _ := config["partlocation"]
+	
+	partsmap = make(map[string]Part)
+
+	err = filepath.Walk(partslocation, walkdirs)
+	errcheck(err)
+
+	fmt.Println(partsmap)
+}
+
+
+func walkdirs(path string , _ os.FileInfo, err error ) (error) {
+	errcheck(err)
+
+	err = filepath.Walk(path, readPartFile)
+	errcheck(err)
+
+	return nil
+}
+
+func readconfig(path string) (error) {
+	
+	configfile, err := os.Open(path)
 
 	config = make(map[string]string)
 	
-	config, err := readconfig("config.stfu")
-	if err != nil {
-		panic(err)
-	}
-
-
-	partfilelocation, ok := config["testfile"]
-	testPart, err := readPartFile(partfilelocation)
-	if err != nil {
-		panic(err)
-	}
-	
-	fmt.Println(testPart)
-}
-
-func readconfig(path string) (map[string]string , error) {
-	configfile, err := os.Open(path)
-
-	var configmap map[string]string
-
-	configmap = new(map[string]string)
-	
-	if err != nil {
-		return configmap, err
-	}
+	errcheck(err)
 	
 	defer configfile.Close()
 
 	scanner := bufio.NewScanner(configfile)
 	for scanner.Scan() {
-		if( strings.Contains(scanner.Text(), "testfile") ){
-			splitstring := strings.Split(scanner.Text(), "=")
-			configmap[splitstring[0]] = splitstring[1]	
-		}
+		splitstring := strings.Split(scanner.Text(), "=")
+		config[splitstring[0]] = splitstring[1]	
 	}
 
-	return configmap, scanner.Err()
+	return  scanner.Err()
 }
 
-func readPartFile(path string) (*Part, error){
+func readPartFile(path string, f os.FileInfo, err error) ( error){
 
-	part := new(Part)
-
-	partfile, err := os.Open(path)
-	if err != nil { 
-		return &Part{}, err
+	if !strings.Contains(path , "part.cfg"){
+		return err
 	}
-
-
+	
+	partfile, err := os.Open(path)
+	errcheck(err)
 
 	defer partfile.Close()
 
+	part := new(Part)
+	name := ""
+	
 	scanner := bufio.NewScanner(partfile)
 	for scanner.Scan() {
-		if( strings.Contains(scanner.Text(), "mass") ){
-			splitstring := strings.Split(scanner.Text(), " ")
+//		fmt.Println(scanner.Text())
+		splitstring := strings.Split(scanner.Text(), " ")
+		switch  splitstring[0] { 
+		default:
+			continue
+		case "name":
+			name = splitstring[2]
+			part.name = name
+		case "mass":
 			part.mass, err = strconv.ParseFloat(splitstring[2], 64)
+		case "maximum_drag":
+			part.drag, err = strconv.ParseFloat(splitstring[2], 64)
 		}
+//		if len(splitstring) > 3 {
+		fmt.Println(splitstring)
+		if strings.Contains(splitstring[0], "maxThrust"){
+			part.thrust, err = strconv.ParseInt(splitstring[2], 10 ,64)
+		}
+//		}
 	}
 
-	return part, scanner.Err()
+	partsmap[name] = *part
+	return  scanner.Err()
 }
 
 
